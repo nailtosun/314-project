4719330e5aa5a528cc3e8b68314808ac05482b6d
DESKTOP-6IJJOUF\nailt <nailtosun07@gmail.com>
2018-04-28 18:12:28 +0300

    first commit

---
+++ SerialToParallel/serial_to_parallel.v
@@ -0,0 +1,15 @@
+//rame gore optimizasyon yapılacak.
+module serial_to_parallel(clk,reset,serial_in,parallel_out);
+input clk, reset, serial_in;
+output reg [11:0] parallel_out;
+initial 
+begin
+parallel_out <= 12'b0;
+end
+always @(posedge clk)
+if (reset == 1)
+begin
+parallel_out <= 12'b0; 
+end
+else parallel_out <= {parallel_out[10:0],serial_in};
+endmodule
